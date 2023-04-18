f2db171e0ae51d5a7610864e3690477da869d6fe
Mash Syed <mash.syed@mashdata.io>
2023-04-18 08:48:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.set_page_config(layout="centered")
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
-# @st.cache_resource
+## @st.cache_resource
 def init_connection():
     return snowflake.connector.connect(**st.secrets["snowflake"])
 
